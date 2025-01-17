36acf0e28dc0ac3b63c0034bee5ea37fbf72233a
Aldan Moldabekuly <aldan.moldabekov@gmail.com>
2025-01-17 11:49:35 +0800

    Create DockerFile

--- app/paymentStatus/page.tsx
+++ app/paymentStatus/page.tsx
@@ -30,7 +30,7 @@ function PaymentStatusContent() {
                         <>
                             <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-red-500 dark:text-red-500">Cancelled</h1>
                             <p className="mb-4 text-3xl tracking-tight font-bold text-gray-200 md:text-4xl dark:text-white">Subscription was cancelled.</p>
-                            <Link href="/" className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-red-900 my-4">Return to Main Page</Link>
+                            <Link href="https://gaiamine.com" className="inline-flex text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-red-900 my-4">Return to Main Page</Link>
                         </>
                     ) : (
                         <>
